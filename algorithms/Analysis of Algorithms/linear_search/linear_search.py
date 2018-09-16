4a7995d49c49c99bd95a6a8103c30564fd867209
michdol <michal.dolbniak@gmail.com>
2018-09-16 10:55:29 +0900

    Linear search - beginning

---
+++ algorithms/Analysis of Algorithms/linear_search/linear_search.py
@@ -0,0 +1,24 @@
+# TODO: get some library to visualise results
+
+
+def linear_search(array, target):
+	for idx, item in enumerate(array, start=1):
+		if item == target:
+			return idx, item
+	return idx, -1
+
+
+number = 30
+best_case = [30, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+avg_case = [1, 1, 1, 1, 1, 1, 30, 1, 1, 1, 1, 1, 1]
+worst_case = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 30]
+
+best_case_result = linear_search(best_case, number)
+avg_case_result = linear_search(avg_case, number)
+worst_case_result = linear_search(worst_case, number)
+
+# print("Length of all arrays: ", len(best_case))
+# print(best_case_result, " O(1)")
+# print(avg_case_result, " O(?)")
+# print(worst_case_result, " O(n)")
+#
---
+++ algorithms/Analysis of Algorithms/linear_search/tmp.py
@@ -0,0 +1,33 @@
+from linear_search import linear_search
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+number = 30
+ranges = [10, 100, 200, 500, 1000]
+cases = [
+	[1 for _ in range(i)] + [30]
+	for i in ranges
+]
+
+# cases = [
+# 	[1 for _ in range(15)] + [30],
+# 	[1 for _ in range(100)] + [30],
+# 	[1 for _ in range(1000)] + [30],
+# ]
+
+results = []
+lens = []
+for case in cases:
+	lens.append(len(case))
+	res = linear_search(case, number)
+	results.append(res[0])
+
+data = {
+	'n': lens,
+	'worst case': results
+}
+df = pd.DataFrame(data)
+df.plot(x='n', y='worst case', style='k--')
+plt.show()
