18ea307780799d5d6b553a32cbc4066355832fc5
michdol <michal.dolbniak@gmail.com>
2018-11-11 08:00:16 +0900

    Max heapify

--- data_structures/heap/structure.py
+++ data_structures/heap/structure.py
@@ -1,8 +1,14 @@
+import pdb
+
+
 class Node(object):
 	def __init__(self, value):
 		self.left = None
 		self.right = None
 		self.value = value
+
+	def __str__(self):
+		return "<Node (%d)>" % self.value
 
 
 def safe_list_item_getter(func):
@@ -42,6 +48,24 @@ class Heap(object):
 	def get_node_by_idx(self, i):
 		return self.nodes[i]
 
+	def max_heapify(self, i):
+		l_idx = self.get_left_index(i)
+		r_idx = self.get_right_index(i)
+		heap_size = len(self.nodes)
+		root_node = self.get_node_by_idx(i)
+		left_node = self.get_node_by_idx(l_idx)
+		right_node = self.get_node_by_idx(r_idx)
+		if l_idx <= heap_size and left_node and root_node and left_node.value > root_node.value:
+			largest = l_idx
+		elif r_idx <= heap_size and right_node and root_node and right_node.value > root_node.value:
+			largest = r_idx
+		else:
+			largest = i
+		# print("largest", largest)
+		if largest != i:
+			self.exchange(largest, i)
+			self.max_heapify(largest)
+
 	def exchange(self, i, j):
 		ith_node = self.get_node_by_idx(i)
 		jth_node = self.get_node_by_idx(j)
@@ -55,20 +79,3 @@ class Heap(object):
 		jth_node.right = i_right
 		self.nodes[i] = jth_node
 		self.nodes[j] = ith_node
-
-	def max_heapify(self, i):
-		l = self.get_left_index(i)
-		r = self.get_right_index(i)
-		heap_size = len(self.nodes)
-		root_node = self.get_node_by_idx(i)
-		left_node = self.get_node_by_idx(l)
-		right_node = self.get_node_by_idx(r)
-		if l <= heap_size and left_node and root_node and left_node.value > root_node.value:
-			largest = l
-		else:
-			largest = i
-		if r <= heap_size and right_node and root_node and right_node.value > root_node.value:
-			largest = r
-		if largest != i:
-			self.exchange(largest, i)
-			self.max_heapify(largest)
--- data_structures/heap/tests.py
+++ data_structures/heap/tests.py
@@ -73,6 +73,31 @@ class HeapTest(TestCase):
 		self.assertEqual(self.heap.nodes[2].value, 3)
 		self.assertIs(self.heap.parent(1).value, 5)
 
+	def test_max_heapify_recursive(self):
+		"""
+					2
+				  /   \
+				 3     4
+				/ \
+			   5   1
+		"""
+		self.root = Node(2)
+		self.root.left = Node(3)
+		self.root.right = Node(4)
+		self.root.left.left = Node(5)
+		self.root.left.right = Node(1)
+
+		self.heap = Heap()
+		self.heap.nodes = [self.root, self.root.left, self.root.right, self.root.left.left, self.root.left.right]
+
+		self.heap.max_heapify(0)
+		self.heap.max_heapify(0)
+		self.assertEqual(self.heap.nodes[0].value, 5)
+		self.assertEqual(self.heap.nodes[1].value, 3)
+		self.assertEqual(self.heap.nodes[2].value, 4)
+		self.assertEqual(self.heap.nodes[3].value, 2)
+		self.assertEqual(self.heap.nodes[4].value, 1)
+
 
 if __name__ == "__main__":
 	main()
