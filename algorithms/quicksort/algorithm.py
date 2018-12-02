50e54b3e97a96c96ef0a9ebb53c59779904cf8d9
michdol <michal.dolbniak@gmail.com>
2018-12-02 09:49:15 +0900

    Randomized quick sort

--- algorithms/quicksort/algorithm.py
+++ algorithms/quicksort/algorithm.py
@@ -1,4 +1,7 @@
 import pdb
+from random import randrange
+from time import sleep
+from timeit import timeit
 
 
 def quick_sort(array, p, r):
@@ -8,11 +11,24 @@ def quick_sort(array, p, r):
 		quick_sort(array, q + 1, r)
 
 
+def randomized_quick_sort(array, p, r):
+	if p < r:
+		q = randomized_partition(array, p, r)
+		randomized_quick_sort(array, p, q - 1)
+		randomized_quick_sort(array, q + 1, r)
+
+
 def exchange(array, i, j):
 	first = array[i]
 	second = array[j]
 	array[i] = second
 	array[j] = first
+
+
+def randomized_partition(array, p, r):
+	i = randrange(p, r)
+	exchange(array, i, r)
+	return partition(array, p, r)
 
 
 def partition(array, p, r):
@@ -43,3 +59,43 @@ def alternative_quick_sort(array):
 		return alternative_quick_sort(less) + equal + alternative_quick_sort(greater)
 	else:
 		return array
+
+
+def test_performance():
+	number = 1000
+	setup_quick_sort = "arr = [2, 8, 7, 1, 3, 5, 6, 4]; from __main__ import quick_sort"
+	quick_sort_result = timeit(
+		"quick_sort(arr, 0, 7)",
+		number=number,
+		setup=setup_quick_sort
+	)
+	sleep(1)
+
+	setup_randomized_quick_sort = "arr = [2, 8, 7, 1, 3, 5, 6, 4]; from __main__ import randomized_quick_sort"
+	randomized_quick_sort_result = timeit(
+		"randomized_quick_sort(arr, 0, 7)",
+		number=number,
+		setup=setup_randomized_quick_sort
+	)
+	sleep(1)
+
+	setup_alternative_quick_sort = "arr = [2, 8, 7, 1, 3, 5, 6, 4]; from __main__ import alternative_quick_sort"
+	alternative_quick_sort_result = timeit(
+		"alternative_quick_sort(arr)",
+		number=number,
+		setup=setup_alternative_quick_sort
+	)
+
+	print(quick_sort_result, "\t\tquick_sort_result")
+	print(randomized_quick_sort_result, "\trandomized_quick_sort_result")
+	print(alternative_quick_sort_result, "\talternative_quick_sort_result")
+	print("\nquick_sort_result / randomized_quick_sort_result")
+	print(quick_sort_result / randomized_quick_sort_result)
+	print("\nquick_sort_result / alternative_quick_sort_result")
+	print(quick_sort_result / alternative_quick_sort_result)
+	print("\nrandomized_quick_sort_result / alternative_quick_sort_result")
+	print(randomized_quick_sort_result / alternative_quick_sort_result)
+
+
+if __name__ == "__main__":
+	test_performance()
--- algorithms/quicksort/tests.py
+++ algorithms/quicksort/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from algorithm import quick_sort, partition, exchange
+from algorithm import quick_sort, partition, exchange, randomized_quick_sort, alternative_quick_sort
 
 
 class QuickSortTest(unittest.TestCase):
@@ -31,6 +31,21 @@ class QuickSortTest(unittest.TestCase):
 		array = [2, 8, 7, 1, 3, 5, 6, 4]
 		quick_sort(array, 0, len(array) - 1)
 		self.assertEqual(array, [1, 2, 3, 4, 5, 6, 7, 8])
+
+	def test_randomized_quick_sort(self):
+		array = [2, 8, 7, 1, 3, 5, 6, 4]
+		randomized_quick_sort(array, 0, len(array) - 1)
+		self.assertEqual(array, [1, 2, 3, 4, 5, 6, 7, 8])
+
+	def test_alternative_quick_sort(self):
+		array = [2, 8, 7, 1, 3, 5, 6, 4]
+		arr = alternative_quick_sort(array)
+		self.assertEqual(arr, [1, 2, 3, 4, 5, 6, 7, 8])
+
+	def test_alternative_quick_sort_single_element(self):
+		array = [2]
+		arr = alternative_quick_sort(array)
+		self.assertEqual(arr, [2])
 
 
 if __name__ == "__main__":
